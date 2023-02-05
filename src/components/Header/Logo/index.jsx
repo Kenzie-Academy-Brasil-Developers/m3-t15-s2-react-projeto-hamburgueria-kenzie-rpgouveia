85793bccf897fbafd4e4dc503abc54c491e99cc6
Renato Gouveia <rpgouveia@gmail.com>
2023-02-05 16:54:31 -0300

    fix: correction for image link for vercel

--- src/components/Header/Logo/index.jsx
+++ src/components/Header/Logo/index.jsx
@@ -1,9 +1,10 @@
 import { StyledImage } from "./styles"
+import BurguerKenzie from "/src/assets/BurguerKenzie.png"
 
 export const Logo = () => {
     return (
         <StyledImage 
-            src="/src/assets/BurguerKenzie.png"
+            src={BurguerKenzie}
             alt="Burguer Kenzie Logo"
         />
     )
