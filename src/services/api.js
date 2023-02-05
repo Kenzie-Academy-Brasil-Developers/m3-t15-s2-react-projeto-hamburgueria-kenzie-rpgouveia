77f3ab2248c98d45d82ce75c0d1dee8aded818f2
Renato Gouveia <rpgouveia@gmail.com>
2023-02-05 16:44:18 -0300

    fix: remove comments and console.logs

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
-// https://hamburgueria-kenzie-json-serve.herokuapp.com/products
 export const api = axios.create({
     baseURL: 'https://hamburgueria-kenzie-json-serve.herokuapp.com/',
     timeout: 5000,
